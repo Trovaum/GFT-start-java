d40d4e78b4443817ba5627bb5dac037c4af3d5c0
Trovaum <ricardopouza@gmail.com>
2022-01-18 13:41:30 -0300

    retorna mensagem

--- src/br/com/dio/Main.java
+++ src/br/com/dio/Main.java
@@ -1,8 +1,20 @@
 package br.com.dio;
 
+import br.com.dio.exercicios.MensagemApartirDoHorario;
+import br.com.dio.exercicios.OperacoesMatematicas;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Operações da matemática!");
+        OperacoesMatematicas.somar(3, 8);
+        OperacoesMatematicas.retirar(9, 5);
+        OperacoesMatematicas.dividir(24, 3);
+        OperacoesMatematicas.multiplicar(20, 4);
+
+        System.out.println("Mensagem -----------");
+        MensagemApartirDoHorario.obterMensagem(10);
+        MensagemApartirDoHorario.obterMensagem(13);
+        MensagemApartirDoHorario.obterMensagem(21);
     }
 }
---
+++ src/br/com/dio/exercicios/MensagemApartirDoHorario.java
@@ -0,0 +1,28 @@
+package br.com.dio.exercicios;
+
+
+public class MensagemApartirDoHorario {
+    public static void obterMensagem(int hora) {
+        switch (hora) {
+            case 6, 7, 8, 9, 10, 11, 12 -> mensagemBomDia();
+            case 13, 14, 15, 16, 17, 18 -> mensagemBoaTarde();
+            case 19, 20, 21, 22, 23, 24, 1, 2, 3, 4, 5 -> mensagemBoaNoite();
+            default -> mensagemInvalida();
+        }
+    }
+
+    public static void mensagemBomDia() {
+        System.out.println("Bom dia!");
+    }
+
+    public static void mensagemBoaTarde() {
+        System.out.println("Boa Tarde!");
+    }
+
+    public static void mensagemBoaNoite() {
+        System.out.println("Boa Noite!");
+    }
+    public static void mensagemInvalida() {
+        System.out.println("Hora invalida!");
+    }
+}
