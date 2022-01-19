3478124c32ce59d1ce28c83255f761abc3b605fa
Trovaum <ricardopouza@gmail.com>
2022-01-18 21:18:32 -0300

    sobrecarga de metodos

--- src/br/com/dio/Main.java
+++ src/br/com/dio/Main.java
@@ -3,6 +3,7 @@ package br.com.dio;
 import br.com.dio.exercicios.CalcularEmprestimo;
 import br.com.dio.exercicios.MensagemApartirDoHorario;
 import br.com.dio.exercicios.OperacoesMatematicas;
+import br.com.dio.exercicios.Quadrilatero;
 
 public class Main {
 
@@ -22,5 +23,10 @@ public class Main {
         CalcularEmprestimo.calcular(100.40, 2);
         CalcularEmprestimo.calcular(40.40, 3);
         CalcularEmprestimo.calcular(20.40, 1);
+
+        System.out.println("Calculando usando metodo de sobrecarga");
+        Quadrilatero.area(4.4);
+        Quadrilatero.area(2.2, 4.8);
+        Quadrilatero.area(8.6, 7.4, 12);
     }
 }
